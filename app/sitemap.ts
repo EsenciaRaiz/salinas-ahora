bf50896da1914601430a539baf3473e6b68bf067
EsenciaRaiz <micaminodesanacion77@gmail.com>
2026-09-16 02:08:04 -0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://salinas-ahora.uruguayrealtors.chatgpt.site/',
+      url: 'https://vitrinacerca.com/',
       lastModified: new Date('2026-09-08'),
       changeFrequency: 'weekly',
       priority: 1,
